5d6466af7b3f122d06f86892f13a503da8a08a2d
thandiwepromise192-arch <thandiwepromise192@gmail.com>
2026-02-01 20:09:15 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 31 19:57:04 2026
+
+@author: : async def func_name(*args, **kwargs):
+    passThandiwe promise mnnisi 
+"""
+
+import streamlit as st
+st.title("Thandiwe promise Mnisi ")
+st.subheader("Coding Summer School 2026")
+st.write("This is my public Streamlit website.")
+
+
+
+
+
+
+
+
+
+ 
